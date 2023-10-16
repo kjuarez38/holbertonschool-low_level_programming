c8657ed78e30987fe3778f7223e63d38ce111a48
kjuarez38 <kaisjuarez@gmail.com>
2023-10-16 01:56:09 -0700

    2-print_alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	int a = 97; 
+	int a = 97;
 	int z = 122;
 	int i;
 
-	for (i = 97 ;i <= z && i >= a; i++)
+	for (i = 97 ; i <= z && i >= a ; i++)
 		putchar(i);
 	putchar('\n');
-	
+
 
 	return (0);
 
