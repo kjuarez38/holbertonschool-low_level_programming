81dc137936900d0bfd14ba47904ebe7f6ac895fe
kjuarez38 <kaisjuarez@gmail.com>
2023-10-16 04:34:49 -0700

    9-print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - description
+ * Return: Always 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9) 
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return 0;
+}
+
