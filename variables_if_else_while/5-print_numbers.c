f8db2ac841b17fdb60c31355ed6f9132e78b1641
kjuarez38 <kaisjuarez@gmail.com>
2023-10-16 03:17:52 -0700

    print nulber

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - description
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 10;
+	int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
