7c0bcc121f683615e9624cbbcf34dd528179f7d3
kjuarez38 <kaisjuarez@gmail.com>
2023-10-16 03:01:04 -0700

    alpaBET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - description
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	
+	for (i = 0 ; i <= 122 ; i++)
+	{
+		if (i >= 97 && i <= 122)
+			putchar(i);
+	}
+	for (i = 0 ; i <= 122 ; i++)
+	{
+		if (i >= 65 && i <= 90)
+                        putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
