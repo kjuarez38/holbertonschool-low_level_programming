c97bd7614700e094015540cfccc4bb87b09ead2f
kjuarez38 <kaisjuarez@gmail.com>
2023-10-16 03:28:41 -0700

    6-print_numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - description
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 57;
+	int i;
+
+	for (i = 48 ; i <= n ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
