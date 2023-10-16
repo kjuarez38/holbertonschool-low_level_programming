c59a1bd1768bc0b3f5810bf87a6a4116f5cdacf8
kjuarez38 <kaisjuarez@gmail.com>
2023-10-16 03:02:12 -0700

    alpaBET

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	
+
 	for (i = 0 ; i <= 122 ; i++)
 	{
 		if (i >= 97 && i <= 122)
@@ -18,7 +18,7 @@ int main(void)
 	for (i = 0 ; i <= 122 ; i++)
 	{
 		if (i >= 65 && i <= 90)
-                        putchar(i);
+			putchar(i);
 	}
 	putchar('\n');
 
