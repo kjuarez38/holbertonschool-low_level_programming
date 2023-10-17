037568602a7818ba1007ccf15ba0c10674ea7d6b
kjuarez38 <kaisjuarez@gmail.com>
2023-10-17 02:50:02 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - main description
+ * @r: variable
+ * Return: Always 0.
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (r * -1);
+	}
+
+	return (r);
+}
