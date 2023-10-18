d64b61cb7cd8656ae5cc0f5dbcaf87e226f96c7c
kjuarez38 <kaisjuarez@gmail.com>
2023-10-18 04:10:51 -0700

    print number

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_most_numbers - check the code.
+*
+* Return: Always 0.
+*/
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48 ; n < 58; n++)
+	{
+		if (n != 50 && n != 52)
+			putchar(n);
+	}
+	putchar('\n');
+}
