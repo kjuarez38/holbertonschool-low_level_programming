dacaeec11dea7314c12e379d58d3051fc64736f0
kjuarez38 <kaisjuarez@gmail.com>
2023-10-18 03:47:40 -0700

    print number

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_numbers - check the code.
+*
+* Return: Always 0.
+*/
+void print_numbers(void)
+{
+	int n;
+
+	for(n = 48 ; n < 58 ; n++)
+		putchar(n);
+		
+	putchar('\n');
+}
