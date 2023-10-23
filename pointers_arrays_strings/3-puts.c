ed12680d9ea382a07302adda29489fc10a40af46
kjuarez38 <kaisjuarez@gmail.com>
2023-10-23 02:29:05 -0700

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _puts - check the code
+ * @str: pointer
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
