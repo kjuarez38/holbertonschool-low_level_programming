e00a70a6bf8403908e26e56a6352ff6e73b2e408
kjuarez38 <kaisjuarez@gmail.com>
2023-10-24 12:03:10 -0700

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -13,16 +13,16 @@ char *leet(char *str)
 {
 	int i;
 	int j;
-	char *arr[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'}
-	int arr2[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1}
+	int arr[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int arr2[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		for (j = 0 ; j <= strlen[arr] ; j++)
+		for (j = 0 ; j <= 10; j++)
 		{
 			if (str[i] == arr[j])
 				str[i] = arr2[j];
 		}
-		putchar(str[i]);
 	}
+	return (str);
 }
