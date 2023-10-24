49808f7ce1ac81778ff7b690fc414db51c912bec
kjuarez38 <kaisjuarez@gmail.com>
2023-10-24 01:45:26 -0700

    puts half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,10 @@ void puts_half(char *str)
 	{
 		len++;
 	}
-	for (i = len / 2   ; i < len   ; i ++)
+	if (len % 2 != 0)
+		len += 1;
+
+	for (i = len / 2   ; i <= len - 1  ; i ++)
 	{
 		putchar(str[i]);
 	}
