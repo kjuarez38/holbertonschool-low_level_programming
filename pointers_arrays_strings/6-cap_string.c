cfcb2f96d65e597c3c9e6ba76a5eb030a026d0ae
kjuarez38 <kaisjuarez@gmail.com>
2023-10-24 11:29:51 -0700

    cap string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,15 +11,20 @@
  */
 char *cap_string(char *str)
 {
+	char *tab = " \n\t,;.!?\"\'(){}";
 	int i = 0;
+	long unsigned int j = 0;
 
 	while (str[i] != '\0')
 	{
-		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+		for (j = 0; j <= strlen(tab) ; j++)
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
+			if (str[i - 1] == tab[j])
 			{
-				str[i] = str[i] - 32;
+				if (str[i] >= 'a' && str[i] <= 'z')
+				{
+					str[i] = str[i] - 32;
+				}
 			}
 		}
 		i++;
