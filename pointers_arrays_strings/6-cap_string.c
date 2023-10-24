996e2dcf11c8a659436d2d7e505b11c095771506
kjuarez38 <kaisjuarez@gmail.com>
2023-10-24 11:14:34 -0700

    cap string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *string_toupper - main description
+ * @str : pointers
+ * Return: Always 1 (True) or 0 (False)
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
