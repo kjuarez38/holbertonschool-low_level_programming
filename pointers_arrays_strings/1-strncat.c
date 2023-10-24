ed7e0c51f7ee3c136921b880fe370b01488258d6
kjuarez38 <kaisjuarez@gmail.com>
2023-10-24 08:40:50 -0700

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcat - point
+ * @dest : pointers
+ * @src : pointer
+ * @n : variable
+ * Return: Always 1 (True) or 0 (False)
+ */
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int len = 0;
+	int len2 = 0;
+	int i = 0;
+
+	while (src[len] != '\0')
+		len++;
+	while (dest[len2] != '\0')
+		len2++;
+
+	for (; i <= len ; i++)
+	{
+		if (i != n)
+			dest[len2 + i] = src[i];
+	}
+
+	return (dest);
+}
+
