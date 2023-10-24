e53fa590e24804c4159c9afcfa0559c194a2fbc0
kjuarez38 <kaisjuarez@gmail.com>
2023-10-24 05:21:00 -0700

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -13,8 +13,10 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	int len = strlen(src);
+	int len = 0;
 
+	while (src[len] != '\0')
+		len++;
 
 	for (i = 0 ; i != len ; i++)
 		dest[i] = src[i];
