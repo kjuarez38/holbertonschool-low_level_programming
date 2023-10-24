5c966d6ded70900eb9ec80749f66665e5bb99470
kjuarez38 <kaisjuarez@gmail.com>
2023-10-24 00:58:57 -0700

    puts half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts_half - check the code
+ * @str: pointer
+ *
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = len / 2 ; i <= len  ; i ++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
