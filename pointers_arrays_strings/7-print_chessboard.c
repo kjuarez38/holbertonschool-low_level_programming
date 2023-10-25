d75d3cbbf253c86d55cad4e9e5f789e79fa90878
kjuarez38 <kaisjuarez@gmail.com>
2023-10-25 08:30:27 -0700

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * print_chessboard - check the code
+ * @(*a)[8]: pointer array
+ *
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
