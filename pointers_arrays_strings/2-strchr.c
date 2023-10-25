8cacd9782c5476c686153723bef611b05b3c4a28
kjuarez38 <kaisjuarez@gmail.com>
2023-10-25 07:06:00 -0700

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
-
+#include <stddef.h>
 /**
  * *_strchr - check the code
  * @s: pointer
@@ -12,7 +12,7 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char s2 = strlen(s);
+	size_t s2 = strlen(s);
 
 	for (i = 0 ; i < s2 ; i++)
 	{
