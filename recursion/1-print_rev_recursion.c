359cee86b7245081152159965de4cda5abd50eac
kjuarez38 <kaisjuarez@gmail.com>
2023-11-04 08:24:56 -0700

    print rev recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - check the code
+ * @s: pointer
+ *
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
