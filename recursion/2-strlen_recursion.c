fc1e711ca6716ca557109ac77654cb4d39094884
kjuarez38 <kaisjuarez@gmail.com>
2023-11-04 08:31:12 -0700

    strlen

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - check the code
+ * @s: pointer
+ *
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
