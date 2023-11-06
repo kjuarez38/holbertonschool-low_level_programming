5a214bd5bcb79fd0348e613412dffb5a90b24c46
kjuarez38 <kaisjuarez@gmail.com>
2023-11-06 05:05:39 -0800

    create an array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,7 +14,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ptr;
 
-	ptr = (char)malloc(size * sizeof(char));
+	ptr = malloc(size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	
 	if (size != 0)
 	{
 		for (i = 0; i < size; i++)
