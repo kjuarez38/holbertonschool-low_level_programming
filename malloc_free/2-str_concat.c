469cd36d818d1499a36c47defe4f5020fd797e8d
kjuarez38 <kaisjuarez@gmail.com>
2023-11-06 05:37:09 -0800

    strncat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,21 +12,18 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int str1, str2, i = 0;
+	int str1 = strlen(s1);
+        int str2 = strlen(s2);
+	int i;
 	char *a;
 
-	if (s1 == NULL)
-                return (NULL);
-        if (s2 == NULL)
-                return (NULL);
+	if (s1 == NULL || s2 == NULL)
+		return NULL;
 
-	str1 = strlen(s1);
-	str2 = strlen(s2);
-
-	a = malloc(str1 + str2 + 1);
+	a = malloc((str1 + str2 + 1) * sizeof(char));
 
 	if (a == NULL)
-		return (NULL);
+		return NULL;
 
 	for (i = 0; i < str1; i++)
 		a[i] = s1[i];
@@ -34,5 +31,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i < str2; i++)
 		a[i + str1] = s2[i];
 
-	return (a);
+	a[str1 + str2] = '\0';
+
+	return a;
 }
