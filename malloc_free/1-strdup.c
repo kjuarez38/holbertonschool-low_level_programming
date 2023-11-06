de93ba1fd82d7ccd23987facfa70e45ed81a9e61
kjuarez38 <kaisjuarez@gmail.com>
2023-11-06 05:34:07 -0800

    strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,19 +13,19 @@ char *_strdup(char *str)
 {
 	char *new_str;
 
-
 	if (str == NULL)
-        {
-                return (NULL);
-        }
+	{
+		return NULL;
+	}
 
-	new_str = (malloc(strlen(str) + 1 * sizeof(char)));
+	new_str = malloc((strlen(str) + 1) * sizeof(char));
 
 	if (new_str == NULL)
-        {
-                return (NULL);
-        }
+	{
+		return NULL;
+	}
 
 	strcpy(new_str, str);
-	return (new_str);
+	return new_str;
 }
+
