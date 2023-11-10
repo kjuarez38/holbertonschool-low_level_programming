c89db575cccc331d322c8d80fcd23c5b32d9b324
kjuarez38 <kaisjuarez@gmail.com>
2023-11-10 04:25:46 -0800

    strnconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -4,18 +4,53 @@
 #include <limits.h>
 
 /**
- * malloc_checked - check the code
+ * string_nconcat - check the code
  *
- * @b: variable
+ * @s1: pointer first word
+ * @s2: pointer second word
+ * @n: variable
  * Return: Always 0.
  */
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+
 {
-	void *ptr;
+	unsigned int len_s1 = 0;
+	unsigned int len_displayed_s2 = 0;
+	char *mal;
+	unsigned int totalSize = 0;
+	unsigned int index = 0;
 
-	ptr = malloc(b);
+	if (s1 == NULL)
+		s1 = "";
 
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len_s1] != '\0')
+		len_s1++;
+
+	while (s2[len_displayed_s2] != '\0')
+		len_displayed_s2++;
+
+	if (n < len_displayed_s2)
+		len_displayed_s2 = n;
+	totalSize = len_s1 + len_displayed_s2;
+
+	mal = malloc(totalSize + 1);
+
+	if (mal == 0)
+	{
+		free(mal);
+		return (NULL);
+	}
+	for (; index < totalSize; index++)
+	{
+		if (index < len_s1)
+			mal[index] = s1[index];
+		else if (index - len_s1 < n)
+			mal[index] = s2[index - len_s1];
+	}
+	mal[index] = '\0';
+
+	return (mal);
 }
