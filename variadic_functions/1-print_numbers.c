b9ec07a57a47a6bdee39cb67a24725dc27e17599
kjuarez38 <kaisjuarez@gmail.com>
2023-11-19 09:16:58 -0800

    print number var_arg

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - sum all the parameters
+ * @separator: you know
+ * @n: variable
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list(args);
+
+	if (separator == NULL)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
