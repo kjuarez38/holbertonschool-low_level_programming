0da7cbf7565bf511b034ffc0b6014e164bf51011
kjuarez38 <kaisjuarez@gmail.com>
2023-11-28 01:39:41 -0800

    add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - Function
+ * @head: linked list
+ * @str: add this new element
+ * Description: print a linked list
+ * Return: length of linked list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (!str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	if (!head)
+		return (0);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = *new_node;
+
+	return (new_node);
+}
