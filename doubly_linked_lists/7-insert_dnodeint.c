4e8ed91d4e55cb89eac24816684c8762b9715766
kjuarez38 <kaisjuarez@gmail.com>
2023-11-30 00:28:48 -0800

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -24,10 +24,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->prev = NULL;
 
 		if (!*h)
-		{
 			(*h)->prev = new_node;
-			new_node->next = *h;
-		}
+
 		*h = new_node;
 		return (new_node);
 	}
