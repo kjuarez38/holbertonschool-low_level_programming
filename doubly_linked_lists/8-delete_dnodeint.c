73aae5587d123eb2cb445a389996b1be2057bb26
kjuarez38 <kaisjuarez@gmail.com>
2023-11-30 02:12:34 -0800

    8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Function
- * @h: node element
- * @idx: place to the new_node
- * @n: int
- * Return: Sum of element in @head.
+ * delete_dnodeint_at_index - Function
+ * @head: node head element
+ * @index: place to delete node
+ * Return: 1. Success
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
@@ -16,29 +15,26 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (*head == NULL)
 		return (-1);
 
-	if (index == 0) {
+	if (index == 0)
+	{
 		*head = (*head)->next;
-		if (*head != NULL) {
+		if (*head != NULL)
 			(*head)->prev = NULL;
-		}
+
 		free(temp);
 		return (1);
 	}
-
-	for (i = 0; i < index - 1 && temp != NULL; i++) {
+	for (i = 0; i < index - 1 && temp != NULL; i++)
 		temp = temp->next;
-	}
 
-	if (temp == NULL || temp->next == NULL) {
+	if (temp == NULL || temp->next == NULL)
 		return (-1);
-	}
 
 	del = temp->next;
 	temp->next = del->next;
 
-	if (del->next != NULL) {
+	if (del->next != NULL)
 		del->next->prev = temp;
-	}
 
 	free(del);
 	return (1);
